167c2bfd87b42a750da5cdba53d73a6726548493
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-03-25 19:39:53 +0000

    Added user routes to server

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require('@koa/cors');
 const app = new Koa();
 app.use(cors());
 
+const users = require('./routes/users.js');
+
+app.use(users.routes());
+
 const port = 3000;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
