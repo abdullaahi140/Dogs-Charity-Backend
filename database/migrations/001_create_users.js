f5da368056bc45f54b931d20e8208cd4ad5c69ee
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-03-29 19:25:25 +0000

    Changed column order

--- database/migrations/001_create_users.js
+++ database/migrations/001_create_users.js
@@ -2,11 +2,11 @@ exports.up = function (knex) {
 	return knex.schema.createTable('users', function (table) {
 		table.increments('ID').unique(),
 		table.string('username').notNullable(),
+		table.string('firstName'),
+		table.string('lastName'),
 		table.string('password').notNullable(),
 		table.string('passwordSalt').notNullable(),
 		table.string('imgURL'),
-		table.string('firstName'),
-		table.string('lastName'),
 		table.string('provider').defaultTo('internal')
 		table.timestamp('dateRegistered').defaultTo(knex.fn.now()),
 		table.timestamp('dateModified').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
