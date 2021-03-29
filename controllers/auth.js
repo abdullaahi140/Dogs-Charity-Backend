c255b8263c75b87fb13590fd3f72d2d8988c1e5c
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-03-29 15:40:51 +0000

    Added Google OAuth 2.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
 const passport = require('koa-passport');
 const basic = require('../strategies/basic.js');
 const jwt = require('../strategies/jwt.js');
+const googleToken = require('../strategies/googleToken.js');
 
 passport.use(basic);
-passport.use(jwt)
+passport.use(jwt);
+passport.use(googleToken)
 
-module.exports = passport.authenticate(['basic', 'jwt'], { session: false });
+exports.auth = passport.authenticate(['basic', 'jwt', 'google-id-token'],
+	{ session: false }
+);
+
+ 
+const google = require('../strategies/google.js');
+passport.use(google);
+
+exports.googleAuth = passport.authenticate(['google'], {
+	session: false,
+	scope: ['profile', 'email'],
+	accessType: 'offline',
+	prompt: 'consent'
+});
