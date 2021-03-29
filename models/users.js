2f3294088303d836758f3281cf0bb4134a633904
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-03-29 15:48:48 +0000

    Added retrieve or add user to database The model has a model that checks if a user is in the database. If it's not then it will add that user and return the recently added row.

--- models/users.js
+++ models/users.js
@@ -5,24 +5,43 @@ exports.getAll = async function getAll() {
 		.leftJoin('roles', 'users.ID', 'roles.userID')
 };
 
-exports.getById = async function getById(id) {
-	return knex.from('users').select('users.*', 'roles.role').where({ ID: id })
+exports.getById = async function getById(id, provider = 'internal') {
+	return knex.from('users').select('users.*', 'roles.role')
+		.where({ ID: id, provider: provider })
 		.leftJoin('roles', 'users.ID', 'roles.userID')
 };
 
-exports.getByUsername = async function getByUsername(username) {
-	return knex.from('users').select('users.*', 'roles.role').where({ username: username })
+exports.getByUsername = async function getByUsername(username, provider = 'internal') {
+	return knex.from('users').select('users.*', 'roles.role')
+		.where({ username: username, provider: provider })
 		.leftJoin('roles', 'users.ID', 'roles.userID')
 };
 
-exports.add = async function add(user) {
+exports.getOrAdd = async function getOrAdd(user) {
+	const result = await module.exports.getByUsername(user.username, user.provider);
+	let data;
+
+	if (result.length) {
+		return result;
+	} else {
+		data = await module.exports.add(user, user.provider);
+	}
+	
+	if (data.length) {
+		const id = data[0]
+		return await module.exports.getById(id, user.provider);
+	}
+}
+
+exports.add = async function add(user, provider = 'internal') {
+	user.provider = provider;
 	return knex.from('users').insert(user);
 };
 
-exports.update = async function update(id, user) {
-	return knex.from('users').update(user).where({ ID: id });
+exports.update = async function update(id, user, provider = 'internal') {
+	return knex.from('users').update(user).where({ ID: id, provider: provider });
 };
 
-exports.delById = async function delById(id) {
-	return knex.from('users').del().where({ ID: id });
+exports.delById = async function delById(id, provider = 'internal') {
+	return knex.from('users').del().where({ ID: id, provider: provider });
 };
