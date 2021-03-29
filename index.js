e162afdc5fd8dfb8d2477319409cff435adfa1fa
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-03-29 19:09:55 +0000

    Changed server port to env variable Will allow the port for the server to be easily changed.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const cors = require('@koa/cors');
+require('dotenv').config()
 
 const app = new Koa();
 app.use(cors());
@@ -10,7 +11,7 @@ const users = require('./routes/users.js');
 app.use(authenticate.routes());
 app.use(users.routes());
 
-const port = 3000;
+const port = process.env.SERVER_PORT;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 })
