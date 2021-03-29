6ccdf6099a02bd86074a3cd2c2471bf809c040fa
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-03-29 15:42:02 +0000

    Added new column and changed primary key New "provider" column has been added to support internal and OAuth accounts. Changed to a composite primary key to allow internal and OAuth accounts to have the same username.

--- database/migrations/001_create_users.js
+++ database/migrations/001_create_users.js
@@ -1,14 +1,16 @@
 exports.up = function (knex) {
 	return knex.schema.createTable('users', function (table) {
 		table.increments('ID').unique(),
-		table.string('username').unique().notNullable(),
+		table.string('username').notNullable(),
 		table.string('password').notNullable(),
 		table.string('passwordSalt').notNullable(),
 		table.string('imgURL'),
 		table.string('firstName'),
 		table.string('lastName'),
+		table.string('provider').defaultTo('internal')
 		table.timestamp('dateRegistered').defaultTo(knex.fn.now()),
 		table.timestamp('dateModified').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+		table.unique(['username', 'provider'])
 	});
 }
 
