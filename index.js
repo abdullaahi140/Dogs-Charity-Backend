f8e6214ee9d2c201095031af584f862b1d8d154e
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-04-02 19:06:08 +0000

    Refactored index.js to be testable Broke out the main logic into app.js file and use import that to start the server. app.js can be used by supertest to simulate API calls.

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const Koa = require('koa');
-const cors = require('@koa/cors');
-require('dotenv').config()
-
-const app = new Koa();
-app.use(cors());
-
-const authenticate = require('./routes/authenticate.js');
-const users = require('./routes/users.js');
-
-app.use(authenticate.routes());
-app.use(users.routes());
+const app = require('./app.js');
+require('dotenv').config();
 
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
+	// eslint-disable-next-line no-console
 	console.log(`Listening on port ${port}`);
-})
+});
