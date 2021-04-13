25fd6b9d3295964b19316005e3319f6836db7a7a
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-04-13 17:12:18 +0100

    Added new routes for messages and chats to app

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const authenticate = require('./routes/authenticate.js');
 const users = require('./routes/users.js');
 const dogs = require('./routes/dogs.js');
 const favDogs = require('./routes/fav_dogs.js');
+const chats = require('./routes/chats.js');
+const messages = require('./routes/messages.js');
 
 app.use(authenticate.routes());
 app.use(users.routes());
 app.use(dogs.routes());
 app.use(favDogs.routes());
+app.use(chats.routes());
+app.use(messages.routes());
 
 module.exports = app;
