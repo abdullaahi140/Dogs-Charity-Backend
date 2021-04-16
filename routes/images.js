33acaa6e082cd58018028725c180cd225e76eeaf
Abdullaahi Farah <faraha11@coventry.ac.uk>
2021-04-16 02:38:46 +0100

    Images route to download images from the server This will use the imageID to find the file of the server and let a request download it.

---
+++ routes/images.js
@@ -0,0 +1,39 @@
+/**
+ * A module to defines routes for getting images from the API.
+ * @module routes/chats
+ * @author Abdullaahi Farah
+ * @see routes/* for other routes in the API
+ */
+
+const Router = require('koa-router');
+const { createReadStream, existsSync } = require('fs');
+
+const model = require('../models/images');
+const { auth } = require('../controllers/auth.js');
+
+/**
+ * Route that gets images using ID from the database
+ * @param {Object} ctx - The Koa request/response context object
+ */
+async function getById(ctx) {
+	const { ID } = ctx.params;
+	const result = await model.getById(ID);
+	if (result.length) {
+		const { filename, type } = result[0];
+		const fileStore = `${__dirname}/../var/tmp/api/public/images`;
+		const path = `${fileStore}/${filename}`;
+		if (existsSync(path)) {
+			const src = createReadStream(path);
+			ctx.type = type;
+			ctx.body = src;
+		}
+	} else {
+		ctx.status = 404;
+	}
+}
+
+const router = Router({ prefix: '/api/v1/images' });
+
+router.get('/:ID([0-9]{1,})', getById);
+
+module.exports = router;
