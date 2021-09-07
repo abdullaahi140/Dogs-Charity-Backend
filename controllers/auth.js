82d7ad668f0e5ea4265081bcd19303a3b0168a95
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-09-07 21:05:44 +0100

    Added error response handling for JWT token errors

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,30 @@ passport.use(jwt);
 passport.use(google);
 
 // Export auth for all routes
-exports.auth = passport.authenticate(['basic', 'jwt'], { session: false });
+exports.auth = async (ctx, next) => (passport.authenticate(['jwt', 'basic'], { session: false }, async (error, user, info) => {
+	if (info) {
+		let [errorInfo] = info;
+		if (Object.keys(info[0]).length === 0) {
+			errorInfo = {
+				name: 'AuthenticationError',
+				message: 'Incorrect username or password'
+			};
+		}
+		ctx.status = 401;
+		// 1st array element has JWT error info
+		ctx.body = errorInfo;
+		return;
+	}
+
+	if (error || !user) {
+		ctx.throw(401, 'Unauthorized');
+	}
+
+	// set the user object to state in Koa context
+	ctx.state.user = user;
+	await next();
+}))(ctx, next);
+
 // Export google authentication for use only on initial login
 exports.googleAuth = passport.authenticate(['google'], {
 	session: false,
