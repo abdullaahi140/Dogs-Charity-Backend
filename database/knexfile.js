2587e5999f2264586b847ea93de877b055e55649
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-09-13 21:43:18 +0100

    Changed prod db config to url

--- database/knexfile.js
+++ database/knexfile.js
@@ -43,13 +43,7 @@ module.exports = {
 	},
 	production: {
 		client: 'mysql2',
-		connection: {
-			host: process.env.DB_HOST,
-			port: process.env.DB_PORT,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_DATABASE
-		},
+		connection: process.env.DB_URL,
 		migrations: {
 			directory: `${__dirname}/migrations`
 		},
