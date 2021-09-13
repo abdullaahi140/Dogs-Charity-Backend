67a3a719fc193259f9df05014ea58699a8072574
Abdullaahi Farah <abdullaahi140@gmail.com>
2021-09-13 20:29:49 +0100

    Added production DB config

--- database/knexfile.js
+++ database/knexfile.js
@@ -40,5 +40,21 @@ module.exports = {
 		seeds: {
 			directory: `${__dirname}/seeds`
 		}
+	},
+	production: {
+		client: 'mysql2',
+		connection: {
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE
+		},
+		migrations: {
+			directory: `${__dirname}/migrations`
+		},
+		seeds: {
+			directory: `${__dirname}/seeds`
+		}
 	}
 };
